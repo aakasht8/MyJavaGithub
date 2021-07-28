76783d65ba7a181ee12dad2dd864ce87c2c62731
aakasht8 <54204762+aakasht8@users.noreply.github.com>
2021-07-28 14:32:51 +0530

    new commit

--- MyJavaGitHub/src/main/java/com/lti/demo/UserApp.java
+++ MyJavaGitHub/src/main/java/com/lti/demo/UserApp.java
@@ -5,6 +5,7 @@ public class UserApp {
 	public static void main(String[] args) {
 		Person p = new Person(101,"Jay",35000);
 		System.out.println(p);
+		System.out.println("Commited changes!!");
 		
 		
 	}
